1f25ee71c014a90a793b22e510086abd81e0e06c
LAPTOP-GQPM5TP9\luigi <luigi.arends@superhshift.nl>
2021-10-11 15:31:09 +0200

    redone login and view

--- src/src/components/Login.vue
+++ src/src/components/Login.vue
@@ -1,14 +1,44 @@
 <template>
-  <div></div>
+  <form class="login" @submit.prevent="handleLogin">
+    <h1>{{ loginHeadlineText }}</h1>
+    <!-- <notification v-if="notifications" :notifications="notifications"></notification> -->
+    <input required :value="email" type="email" placeholder="{{ loginEmailPlaceholder }}" />
+    <input required :value="password" type="password" placeholder="{{ loginPasswordPlaceholder }}" />
+    <button class="btn right" type="submit">{{ loginButtonText }}</button>
+    <a href="#/forgot">{{ loginForgotPasswordText }}</a>
+    <a href="#/reset">{{ loginResetPasswordText }}</a>
+  </form>
 </template>
 
 <script>
-import { defineComponent } from "@vue/runtime-core";
+import { computed, defineComponent } from 'vue';
+import store from "../store/index";
 
 export default defineComponent({
     name: 'Login',
     setup(){
-        return {};
+        const email = ref("");
+        const password = ref("");
+        const loginHeadlineText = computed(() => store.getters.loginHeadlineText);
+        const loginButtonText = computed(() => store.getters.loginButtonText);
+        const loginEmailPlaceholder = computed(() => store.getters.loginEmailPlaceholder);
+        const loginPasswordPlaceholder = computed(() => store.getters.loginPasswordPlaceholder);
+        const loginForgotPasswordText = computed(() => store.getters.loginForgotPasswordText);
+        const loginResetPasswordText = computed(() => store.getters.loginResetPasswordText);
+        function handleLogin() {
+            console.log('TODO: Fix login');
+        }
+        return {
+            email,
+            password,
+            loginHeadlineText,
+            loginButtonText,
+            loginResetPasswordText,
+            loginForgotPasswordText,
+            loginEmailPlaceholder,
+            loginPasswordPlaceholder,
+            handleLogin,
+        };
     },
 });
 </script>
---
+++ src/src/view/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <Login />
+  </article>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import Login from '../components/Login.vue';
+
+export default defineComponent({
+    name: 'login-view',
+    components:{
+      Login,
+    },
+    setup(){
+        return {};
+    },
+});
+</script>
+
+<style scoped lang="scss"></style>
