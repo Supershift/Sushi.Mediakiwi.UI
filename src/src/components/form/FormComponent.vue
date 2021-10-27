0101b5c344328e80ffaa4dd8283f511f573732a9
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2021-10-27 11:21:47 +0200

    moved rows from function to once in setup

--- src/src/components/form/FormComponent.vue
+++ src/src/components/form/FormComponent.vue
@@ -1,52 +1,22 @@
 <template>
   <section>
+    <p>Hallo wereld</p>
+
     <table class="formTable">
       <tbody>
-        <tr
-          v-for="row in rows()"
-          :key="row.id"
-        >
+        <tr v-for="row in rowArray" :key="row.id">
           <td v-if="row.isButtonRow">
-            <component
-              :is="checkVueType(field)"
-              v-for="field in row.fields"
-              :key="field.propertyName"
-              v-bind="field"
-              @onclick="handleButtonClicked"
-            />
+            <component :is="checkVueType(field)" v-for="field in row.fields" :key="field.propertyName" v-bind="field" @onclick="handleButtonClicked" />
           </td>
-          <template
-            v-for="field, in row.fields"
-            v-else
-          >
-            <component
-              :is="checkVueType(field)"
-              v-if="checkVueType(field) === 'FormSection'"
-              :key="field.propertyName"
-              v-model="field.value"
-              v-bind="field"
-            />
+          <template v-for="field in row.fields" v-else>
+            <component :is="checkVueType(field)" v-if="checkVueType(field) === 'FormSection'" :key="field.propertyName" v-model="field.value" v-bind="field" />
             <template v-else>
-              <th
-                v-show="showField(field)"
-                :key="field.propertyName"
-                :class="expressCell(field.expression)"
-                colspan="1"
-              >
-                <label
-                  v-if="!hideLabelForType(field.vueType)"
-                  :for="field.propertyName"
-                  :title="field.title"
-                  :class="{ 'mandatory': field.mandatory }"
-                >
+              <th v-show="showField(field)" :key="field.propertyName" :class="expressCell(field.expression)" colspan="1">
+                <label v-if="!hideLabelForType(field.vueType)" :for="field.propertyName" :title="field.title" :class="{ mandatory: field.mandatory }">
                   {{ field.title }}
                 </label>
               </th>
-              <td
-                :key="field.propertyName"
-                :class="expressCell(field.expression)"
-                :colspan="getColspan(row)"
-              >
+              <td :key="field.propertyName" :class="expressCell(field.expression)" :colspan="getColspan(row)">
                 <component
                   :is="checkVueType(field)"
                   v-if="field.vueType === 'FormButton'"
@@ -72,7 +42,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, reactive, ref, } from "vue";
+import { defineComponent, PropType, reactive, ref } from "vue";
 import FieldModel from "../../models/FieldModel";
 import FormRowModel from "../../models/FormRowModel";
 import MessageModel from "../../models/MessageModel";
@@ -91,18 +61,18 @@ import FormSection from "./FormSection.vue";
 
 export default defineComponent({
   name: "FormComponent",
-  mixin: [ ExpressionType, fieldMixins ],
-  components:{
-    CustomButton,
-    CustomInput,
-    CustomChoiceCheckBox,
-    CustomSelectInput,
-    CustomChoiceRadio,
-    CustomSection,
-    CustomChoiceDropdown,
-    FormButton,
-    FormInput,
-    FormSection,
+  mixin: [ExpressionType, fieldMixins],
+  components: {
+    // CustomButton,
+    // CustomInput,
+    // CustomChoiceCheckBox,
+    // CustomSelectInput,
+    // CustomChoiceRadio,
+    // CustomSection,
+    // CustomChoiceDropdown,
+    // FormButton,
+    // FormInput,
+    // FormSection,
   },
   props: {
     fields: {
@@ -112,9 +82,9 @@ export default defineComponent({
     notifications: {
       type: Object as PropType<Array<MessageModel>>,
       required: true,
-    }
+    },
   },
-  emits: ["toggle","addFields", "removeFields", "fieldChanged", "buttonClicked"],
+  emits: ["toggle", "addFields", "removeFields", "fieldChanged", "buttonClicked"],
   setup(props, context) {
     let rowArray = ref<Array<FormRowModel>>([]);
     function checkVueType(field: FieldModel): string {
@@ -142,38 +112,40 @@ export default defineComponent({
       }
     }
     function clean(label: string): string {
-      return label.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+      return label
+        .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
           return index === 0 ? word.toLowerCase() : word.toUpperCase();
-      }).replace(/\s+/g, "");
+        })
+        .replace(/\s+/g, "");
     }
     function expressCell(expression: ExpressionType) {
-      return (expression !== ExpressionType.Full) ? "vhalf" : "full";
+      return expression !== ExpressionType.Full ? "vhalf" : "full";
     }
     function getColspan(row: FormRowModel) {
       const threeCols = 3;
       const oneCol = 1;
       if (!row) {
-          return 1;
+        return 1;
       }
 
       // With 2 fields on single row the TD has a colspan of 1
       // With 1 field on single row; the TD has a colspan of 3 (4 minus the td title)
-      return (row.fields.length === 1) ? threeCols : oneCol;
+      return row.fields.length === 1 ? threeCols : oneCol;
     }
     function showField(field: FieldModel) {
-        if (field.hidden) {
-            return false;
-        }
+      // if (field.hidden) {
+      //   return false;
+      // }
 
-        if (field.vueType === "wimSection") {
-            return true;
-        }
+      // if (field.vueType === "wimSection") {
+      //   return true;
+      // }
 
-        if (!field.hidden) {
-            return false;
-        }
+      // if (!field.hidden) {
+      //   return false;
+      // }
 
-        return true;
+      return true;
     }
     function handleToggle(section: SectionModel) {
       context.emit("toggle", section);
@@ -191,43 +163,45 @@ export default defineComponent({
       context.emit("buttonClicked", e, field);
     }
     function hideLabelForType(vueType: string) {
-      return (vueType === "wimButton") ? true : false;
+      return vueType === "wimButton" ? true : false;
     }
     function isHalfField(expression: ExpressionType) {
-        return (expression !== ExpressionType.Full) ? " half" : " long";
+      return expression !== ExpressionType.Full ? " half" : " long";
     }
-    function rows() {
-      //let rowArray = Array<GridRowModel>();
-      // rows have an array of fields with max. 2 items
-      // one left one right
-      
-      if (!props.fields || !props.fields.length) {
-        return rowArray;
-      }
 
-      let currentFormSection = "baseSection";
-      
-      let count = 0;
-      for (let field of props.fields) {
-        count++;
-        /* eslint no-console:0*/
+    // // function rows() {
+    // // let rows = null;
+    // //let rowArray = Array<GridRowModel>();
+    // // rows have an array of fields with max. 2 items
+    // // one left one right
+
+    // if (!props.fields || !props.fields.length) {
+    //   return rowArray;
+    // }
+
+    let currentFormSection = "baseSection";
+
+    let count = 0;
+    for (let field of props.fields) {
+      count++;
+      /* eslint no-console:0*/
       console.log(props.fields);
-        
-        let initalRow = ref<FormRowModel>({
+
+      let initalRow = ref<FormRowModel>({
         fields: [],
       });
       //   const keyGen = 621355968000000000;
       //   const keyMultiply = 10000;
-        const cellLimit = 2;
+      const cellLimit = 2;
       //   // set componentKey; This should update every time the field is updated
       //   field.componentKey = new Date().getTime() * keyMultiply + keyGen;
 
-        if (!field.formSection) {
-          if (field.vueType === vueTypes.formSection) {
-            currentFormSection = `${clean(field.title)}_${props.fields.indexOf(field)}`;
-          }
-          field.formSection = currentFormSection;
+      if (!field.formSection) {
+        if (field.vueType === vueTypes.formSection) {
+          currentFormSection = `${clean(field.title)}_${props.fields.indexOf(field)}`;
         }
+        field.formSection = currentFormSection;
+      }
 
       //   // Check if there is a notification for this field
       //   if (props.notifications && props.notifications.length) {
@@ -238,28 +212,31 @@ export default defineComponent({
       //   }
 
       //   // is this a full width field?
-        if (field.expression === ExpressionType.Full) {
-          // add the current row object to the list
-          if (initalRow.value.fields.length) {
-            // initalRow.isButtonRow = initalRow.fields.map((mappedField) => mappedField.vueType).every((val) => val === vueTypes.formButton);
-            rowArray.value.push(initalRow.value);
-          }
-          // create a new row
-          initalRow.value = { fields: [] };
-          // add push it to the list
-          initalRow.value.fields.push(field); 
-        } else {
-          if (initalRow.value.fields.length >= cellLimit && initalRow.value.fields.map((mappedField) => mappedField.vueType).every((val) => val === vueTypes.formButton)) {
-            if (field.vueType !== vueTypes.formButton) {
-              // initalRow.isButtonRow = true;
-              rowArray.value.push(initalRow.value);
-              // create a new row
-              initalRow.value = { fields: [] };
-            }
-          }
-      //     // add to 'current' row
-          initalRow.value.fields.push(field);
+      if (field.expression === ExpressionType.Full) {
+        // add the current row object to the list
+        if (initalRow.value.fields.length) {
+          // initalRow.isButtonRow = initalRow.fields.map((mappedField) => mappedField.vueType).every((val) => val === vueTypes.formButton);
+          rowArray.value.push(initalRow.value);
         }
+        // create a new row
+        initalRow.value = { fields: [] };
+        // add push it to the list
+        initalRow.value.fields.push(field);
+      } else {
+        if (
+          initalRow.value.fields.length >= cellLimit &&
+          initalRow.value.fields.map((mappedField) => mappedField.vueType).every((val) => val === vueTypes.formButton)
+        ) {
+          if (field.vueType !== vueTypes.formButton) {
+            // initalRow.isButtonRow = true;
+            rowArray.value.push(initalRow.value);
+            // create a new row
+            initalRow.value = { fields: [] };
+          }
+        }
+        //     // add to 'current' row
+        initalRow.value.fields.push(field);
+      }
       //   // add row to rows
       //   // when its 1 fullwith row
       //   // or there are 2 rows added
@@ -291,13 +268,14 @@ export default defineComponent({
       // // add last row
       // if (initalRow.fields.length > 0) {
       //   initalRow.isButtonRow = initalRow.fields.map((r) => r.vueType).every((val) => val === vueTypes.formButton);
-        rowArray.value.push(initalRow.value);
-      }
-
-      return rowArray;
+      rowArray.value.push(initalRow.value);
     }
+
+    alert("yoyo");
+    // return rowArray;
+    // }
     return {
-      rows,
+      rowArray,
       expressCell,
       getColspan,
       showField,
@@ -310,6 +288,9 @@ export default defineComponent({
       isHalfField,
       checkVueType,
     };
+  },
+  created() {
+    alert("create me");
   },
 });
 </script>
