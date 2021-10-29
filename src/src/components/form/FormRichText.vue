ef800895f6ae9b5b93bc413f5b2f7c642415f2f9
Jean-Paul Kommers <jeanpaul.kommers@supershift.nl>
2021-10-29 15:36:32 +0200

    Update FormRichText.vue

--- src/src/components/form/FormRichText.vue
+++ src/src/components/form/FormRichText.vue
@@ -1,81 +1,74 @@
 <template>
-  <div :class="customRichtextContainerClasses">
-    <editor
-      v-model="valueRef"
-      :id="fieldID"
-      :name="fieldID"
-      toolbar= "bold italic underline bullist numlist indent outdent link unlink removeformat subscript superscript code"
-      :class="customRichtextClasses"
-      :disabled="richtext.disabled || richtext.readOnly"
-      v-on="localEventHandler('handleChange')"
-    />
-  </div>
+    <div :class="customRichtextContainerClasses">
+        <editor
+            api-key="4w7n1fpw2lpcb86gkmkqfpsrefktlebicr8wc1lzwuy0evy4"
+            :id="richtext.propertyName"
+            :init="tinymceInit"
+            v-model="valueRef"
+            :class="customRichtextClasses"
+            :disabled="richtext.disabled || richtext.readOnly"
+            @blur="handleChange"
+        />
+    </div>
 </template>
 <script lang="ts">
 // import Editor from '@tinymce/tinymce-vue';
 import { defineComponent, PropType, ref, computed, reactive } from "vue";
-import OptionModel from "../../models/OptionModel";
 import FieldModel from "../../models/FieldModel";
 import Editor from "@tinymce/tinymce-vue";
-
 
 export default defineComponent({
     name: "RichText",
     components: {
-      "editor": Editor,
+        editor: Editor,
     },
     props: {
-      richtext: {
-        type: Object as PropType<FieldModel>,
-        required: true,
-      },
-      classname: {
-        type: String,
-        required: true,
-      },
+        richtext: {
+            type: Object as PropType<FieldModel>,
+            required: true,
+        },
+        classname: {
+            type: String,
+            required: true,
+        },
     },
     emits: ["valueChanged"],
-    setup(props, context){
-      let valueEvent = ref("");
-      let valueRef = ref("");
-      const tinymce = reactive({
-        language: `${props.richtext.locale}` ? `${props.richtext.locale}` : "en",
-        menubar: false,
-        statusbar: false,
-        plugins: ["code link"],
-      });
-      const customRichtextContainerClasses = computed(() => ["richtext-container ", props.richtext?.className]);
-      const customRichtextClasses = computed(() => ["richtext-primary ", props.classname]);
-      const fieldID = computed((option: OptionModel) => {
-        return `${props.richtext?.value}_${props.richtext?.propertyName}_${option?.value}`;
-      });
-      function handleChange(values: string) {
-        if (props.richtext?.event !== "none") {
-          context.emit("valueChanged", values);
+    setup(props, context) {
+        let valueRef = ref("");
+
+        const customRichtextContainerClasses = computed(() => [
+            "richtext-container ",
+            props.richtext?.className,
+        ]);
+        const customRichtextClasses = computed(() => [
+            "richtext-primary ",
+            props.classname,
+        ]);
+
+        function handleChange(values: string) {
+            if (props.richtext?.event !== "none") {
+                context.emit("valueChanged", values);
+            }
         }
-      }
-      function localEventHandler(method: string) {
-        switch (props.richtext?.event) {
-          case "blur":
-            valueEvent.value = "onBlur";
-            //props.richtext.event = "onBlur";
-            break;
-          default:
-            break;                     
-        }
-        return method; //this.eventHandler(this.field, method);
-      }
-      return {
-        fieldID,
-        handleChange,
-        localEventHandler,
-        customRichtextClasses,
-        customRichtextContainerClasses,
-        tinymce,
-        valueRef,
-      };
+        const tinymceInit = {
+            menubar: false,
+            statusbar: false,
+            plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table paste code help wordcount",
+            ],
+            toolbar:
+                "bold italic underline bullist numlist indent outdent link unlink removeformat subscript superscript code",
+        };
+
+        return {
+            handleChange,
+            customRichtextClasses,
+            customRichtextContainerClasses,
+            valueRef,
+            tinymceInit,
+        };
     },
 });
 </script>
-
-<style scoped lang="scss"></style>
